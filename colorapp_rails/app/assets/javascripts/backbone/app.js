b990c4b842ba965e0edd51bec66a5dd1005fdff0
AdrianMLin <adrian.mx.lin@gmail.com>
2014-10-25 20:21:48 -0400

    SORTING: set up function to sort by color, collections are being made, but list view doesn't get created

--- colorapp_rails/app/assets/javascripts/backbone/app.js
+++ colorapp_rails/app/assets/javascripts/backbone/app.js
@@ -6,7 +6,7 @@ Colorapp.initialize = function(){
 	appCollection = new Colorapp.Collections.AppCollection();
 
 	var listView = new Colorapp.Views.AppListView({
-		collection: appCollection,
+		collection: appCollection
 		// el: $('.penguin-list') // i think i'm generating an article, then appending to main
 	});
 
@@ -20,11 +20,31 @@ Colorapp.initialize = function(){
 	// 	$('input').val('')
 	// 	collection.create({name: penguinName})
 	// })
+
+	setUpColorNavbar();
+
+
+}
+
+
+function setUpColorNavbar(){
+	//setting up the color navbar to sort colors
+	$('.color').on('click', function(){
+		var color = this.id
+		console.log(color) //black, blue etc
+
+		var colorSortedApps = appCollection.where({color1: color})
+		var listView = new Colorapp.Views.AppListView({
+			collection: colorSortedApps
+		});
+
+		$('#main-content-area').append(listView.el) // do i need this?
+	});
 }
 
 
 $(function(){
-	console.log($('#app-template'))
 	Colorapp.initialize();
-
 });
+
+
--- colorapp_rails/app/assets/javascripts/backbone/views/AppListView.js
+++ colorapp_rails/app/assets/javascripts/backbone/views/AppListView.js
@@ -3,6 +3,7 @@ var Colorapp = Colorapp || { Models: {}, Collections: {}, Views: {} };
 Colorapp.Views.AppListView = Backbone.View.extend({
 	initialize: function(){
 		this.listenTo(this.collection, 'add', this.render);
+		this.listenTo(this.collection, 'change', this.render);
 	},
 	
 	tagName: 'article',
@@ -10,7 +11,7 @@ Colorapp.Views.AppListView = Backbone.View.extend({
 
 	render: function(){
 		var self = this;
-		this.$el.empty();
+		this.$el.empty(); // clears the el so it can be repopulated
 		_.each(this.collection.models, function(app){
 			var appView = new Colorapp.Views.AppView({model: app})
 			self.$el.append( appView.render().el );
